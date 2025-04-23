ad6b1bf296327fc6ca345f381d1507be9c5cfba0
GretaBetty <bettellagreta@gmail.com>
2025-04-23 20:07:04 +0000

    update code

--- src/pages/UserProfilePage.tsx
+++ src/pages/UserProfilePage.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
+
 import { supabase } from "../supabaseClient";
 
 export default function ChatPage() {
